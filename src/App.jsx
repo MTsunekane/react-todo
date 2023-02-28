2f1399879de747cccea27ec6794991e849ff02d2
MTsunekane <mizuki.tsunekane@gmail.com>
2023-02-28 07:20:44 +0000

    Update App.jsx and styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 import "./styles.css";
 
 export const App = () => {
-  return <p>ああ</p>;
+  //todoの入力のstate
+  const [todoText, setTodoText] = useState("");
+  //未完了のTODOのstate
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  //完了のTODOのstate
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  //inputに文字を入力できるようにする
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  //入力した値を、「未完了のTODO」に追加する、空白の時は処理を実行しない
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  //削除ボタンを押したときの動作、spliceでindex（番号）のtodoを第二引数の数だけ削除する
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  //完了ボタンを押したときに、未完了のTODOから、完了のTODOに移動させる
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  //戻すボタンを押したときに、完了のTODOから未完了のTODOに戻す
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
 };
---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const IncompleteTodos = (props) => {
+  const { todos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="incomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {/* incompleteaTodosからtodoを引数にとってmapで繰り返し処理を行う、第二引数にindexを渡すと、何番目のtodo
+かを教えてくれるよ */}
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
